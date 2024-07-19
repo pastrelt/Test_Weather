f41f0f170718b6985e56ab1beaf012f4f06d7f29
pastrelt <pavel.streltsov13@gmail.com>
2024-07-19 18:45:58 +0700

    добавляем новый проект в githab

--- Django_views/simpleapp/forms.py
+++ Django_views/simpleapp/forms.py
@@ -1,20 +1,33 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import Product
 
-class ProductForm(forms.ModelForm):
-   class Meta:
-       model = Product
-       # особенное значение ’__all__’, которое означает, что Django сам должен пойти в модель и взять все поля,
-       # кроме первичного ключа
-       fields = '__all__'
 
-# class ProductForm(forms.ModelForm):
-#    class Meta:
-#        model = Product
-#        fields = [
-#            'name',
-#            'description',
-#            'quantity',
-#            'category',
-#            'price',
-#        ]
+class ProductForm(forms.ModelForm):
+    description = forms.CharField(min_length=20)
+
+    class Meta:
+        model = Product
+        fields = [
+            'name',
+            'description',
+            'category',
+            'price',
+            'quantity',
+        ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get("name")
+        description = cleaned_data.get("description")
+
+        if name == description:
+            raise ValidationError(
+                "Описание не должно быть идентичным названию."
+            )
+
+        return cleaned_data
+# особенное значение ’__all__’, которое означает, что Django сам должен пойти в модель и взять все поля,
+# кроме первичного ключа
+#fields = '__all__'
--- pas.py
+++ pas.py
@@ -1,13 +1,18 @@
-from django.db import models  # импорт
-
-
-class Author(models.Model):
-    full_name = models.CharField(max_length=64)
-    name = models.CharField(null=True, max_length=64)
-
-    def some_method(self):
-        self.name = self.full_name.split()[0]
-
-    self.save()
-
-Author.some_method()
+#string = input("Введите числа через пробел:")
+string = input("Введите числа через пробел:").split()
+print(string)
+# f = string[0:1]
+# #print(f)
+# end_s = string[-1:]
+# #print(end_s)
+# string.pop(0)
+# string.pop(-1)
+# print(string)
+# string.append(f)
+# print(string)
+# string.append(end_s)
+# print(string)
+string[0], string[-1] = string[-1], string[0]
+#sum_s = sum(map(int, string))
+string.append(sum(map(int, string)))
+print(string)
